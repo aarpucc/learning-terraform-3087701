3772c3cddf41f296a45d07c7749f82a04fa41a54
Bruno Fernandes <162385760+aarpucc@users.noreply.github.com>
2025-06-18 15:47:15 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.root.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.root.arn
+  value = aws_instance.blog.arn
 }
