a32777ae5525a0d175e1098a0c289cdf6d3a9bf2
Bruno Fernandes <162385760+aarpucc@users.noreply.github.com>
2025-06-23 15:25:54 +0100

    auto scaling

--- main.tf
+++ main.tf
@@ -24,26 +24,30 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
-resource "aws_instance" "blog" {
-  ami = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.3.0"
+
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier    = module.blog_vpc.public_subnets
+  target_group_arns      = module.blog_alb.target_group_arns
+  security_group         = [module.blog_sg.security_group_id]
+  
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-
+  # insert the 1 required variable here
 }
- 
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
