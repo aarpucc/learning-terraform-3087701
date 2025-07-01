96e9b39e1f639870cd5aeaa2b2b41cb9b5b15471
Bruno Fernandes <162385760+aarpucc@users.noreply.github.com>
2025-07-01 09:54:06 +0100

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,52 +11,48 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Terraform = "true"
+    Environment = var.environment.name
   }
 }
 
-module "autoscaling" {
+
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.3.0"
+  version = "6.5.2"
 
-  name     = "blog"
-  min_size = 1
-  max_size = 2
+  name = "${var.environment.name}-blog"
+  min_size            = var.asg_min_siza
+  max_size            = var.asg_max_size
 
-  vpc_zone_identifier    = module.blog_vpc.public_subnets
-  security_groups        = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
   
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+   image_id            = data.aws_ami.app_ami.id
+   instance_type       = var.instance_type
 }
-
-resource "aws_autoscaling_attachment" "this" {
-  autoscaling_group_name = module.autoscaling.autoscaling_group_name
-  target_group_arn   = module.blog_alb.target_group_arns[0]
-}
-
-
-
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -66,11 +62,11 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
-      target_type      = "instance"    
-      }
+      target_type      = "instance"
+    }
   ]
 
   http_tcp_listeners = [
@@ -82,7 +78,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -90,8 +86,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
+  
+  name    = "${var.environment.name}-blog"
   vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
